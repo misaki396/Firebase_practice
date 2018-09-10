2a6abe4e7424378a27ecf2340ff4201c52d66307
徳藤美咲 <tokufujimisaki@tokufujimisakinoMacBook.local>
2018-09-10 15:35:22 +0900

    sai_ignore

--- app/assets/javascripts/initialize.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyB1lroGMqKEHGfiKN0hPLARZP19fIyc1gw",
-    authDomain: "yokin-tsucho.firebaseapp.com",
-    databaseURL: "https://yokin-tsucho.firebaseio.com",
-    projectId: "yokin-tsucho",
-    storageBucket: "yokin-tsucho.appspot.com",
-    messagingSenderId: "380442723248"
-};
-firebase.initializeApp(config);
